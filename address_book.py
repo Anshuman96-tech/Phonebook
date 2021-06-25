47a4e4095a8fc1a373d0a164206dc060e719f1b2
Anshuman96-tech <84167799+Anshuman96-tech@users.noreply.github.com>
2021-06-25 20:10:23 +0530

    Update address_book.py

--- address_book.py
+++ address_book.py
@@ -1,7 +1,5 @@
-#import library
 from tkinter import *
 
-#initialize window
 root = Tk()
 root.geometry('400x400')
 root.config(bg = 'SlateGray3')
@@ -19,8 +17,6 @@ contactlist = [
 Name = StringVar()
 Number = StringVar()
 
-
-#create frame
 frame = Frame(root)
 frame.pack(side = RIGHT)
 
@@ -31,38 +27,29 @@ scroll.pack(side=RIGHT, fill=Y)
 select.pack(side=LEFT,  fill=BOTH, expand=1)
 
 
-########### function to get select value
-
 def Selected():
     return int(select.curselection()[0])
     
-##fun to add new contact
 def AddContact():
     contactlist.append([Name.get(), Number.get()])
     Select_set()
 
-# fun to edit existing contact(first select the contact then click on view button then edit the contact and then click on edit button)
 def EDIT():
     contactlist[Selected()] = [Name.get(), Number.get()]
     Select_set()
     
-#to delete selected contact
 def DELETE():
     del contactlist[Selected()]
     Select_set()
    
-# to view selected contact(first select then click on view button)
 def VIEW():
     NAME, PHONE = contactlist[Selected()]
     Name.set(NAME)
     Number.set(PHONE)
-
-
-###exit game window   
+   
 def EXIT():
     root.destroy()
 
-#empty name and number field
 def RESET():
     Name.set('')
     Number.set('')
@@ -75,9 +62,6 @@ def Select_set() :
         select.insert (END, name)
 Select_set()
 
-
-
-######define buttons #####labels and entry widget
 Label(root, text = 'NAME', font='arial 12 bold', bg = 'SlateGray3').place(x= 30, y=20)
 Entry(root, textvariable = Name).place(x= 100, y=20)
 Label(root, text = 'PHONE NO.', font='arial 12 bold',bg = 'SlateGray3').place(x= 30, y=70)
